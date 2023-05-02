cb263055f2b63164f4c1d996e195e340e16014f6
Damian <damian.flexsoft@gmail.com>
2023-05-02 20:25:21 +0200

    Sigmoid function for logistic regresseion

---
+++ MachineLearning/Classification/SigmoidFunciton.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+input_array = np.array([1,2,3])
+exp_array = np.exp(input_array)
+
+print("Input to exp:", input_array)
+print("Output of exp:", exp_array)
+
+# Input is a single number
+input_val = 1  
+exp_val = np.exp(input_val)
+
+print("Input to exp:", input_val)
+print("Output of exp:", exp_val)
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+         
+    """
+    return 1/(1+np.exp(-z))
+
+
+# Generate an array of evenly spaced values between -10 and 10
+z_tmp = np.arange(-10,11)
+
+# Use the function implemented above to get the sigmoid values
+y = sigmoid(z_tmp)
+
+# Code for pretty printing the two arrays next to each other
+np.set_printoptions(precision=3) 
+print("Input (z), Output (sigmoid(z))")
+print(np.c_[z_tmp, y])
