f136ce4ee81aaab406b1a40aca24dea19b341878
Damian <damian.flexsoft@gmail.com>
2023-07-31 12:56:59 +0200

    Kubectl installation

--- AnalyzingDataWithPython/Python/test2.sh
+++ AnalyzingDataWithPython/Python/test2.sh
@@ -1,1 +1,11 @@
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+
+sudo apt-get install -y apt-transport-https
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
